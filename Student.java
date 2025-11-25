8e08c08b6dde726c5518c487b49d6aafdc254902
Madhusmita2004 <157032852+Madhusmita2004@users.noreply.github.com>
2025-11-25 23:33:54 +0530

    Replace course with marks in Student class

--- Student.java
+++ Student.java
@@ -2,50 +2,47 @@ public class Student {
     private int id;
     private String name;
     private int age;
-    private String course;
+    private double marks;
 
-    public Student(int id, String name, int age, String course) {
+    public Student(int id, String name, int age, double marks) {
         this.id = id;
         this.name = name;
         this.age = age;
-        this.course = course;
+        this.marks = marks;
     }
 
-    // Getters & Setters
+    // Getters
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
         this.age = age;
     }
 
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
+    public void setMarks(double marks) {
+        this.marks = marks;
     }
 
     @Override
     public String toString() {
-        return id + " | " + name + " | " + age + " | " + course;
+        return id + " | " + name + " | " + age + " | " + marks;
     }
 }
